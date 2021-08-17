7d96d58e0e52e641f36ae52fbf4ee41240129d8a
Michael Brodskiy <Michael@Brodskiy.com>
2021-08-17 10:03:41 -0700

    added directories

---
+++ Senior Year/AP Computer Science/Unit One/Assignments/HelloWorldName.java
@@ -0,0 +1,28 @@
+/*
+ *
+ * Written by: Michael Brodskiy
+ * Class: AP Computer Science A
+ * Instructor: Mr. Davis
+ *
+ */
+
+/**
+ * This program uses two print statements and two println statements
+ * */
+
+public class HelloWorldName
+
+{
+
+    public static void main(String[] args)
+    
+    {
+
+        System.out.print("Hello");
+        System.out.println(" World");
+        System.out.print("By");
+        System.out.println(" Michael");
+
+    }
+
+}
