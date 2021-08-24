f93ffbd8e51e07bdcce7e46066dbcf3fb6454291
Michael Brodskiy <Michael@Brodskiy.com>
2021-08-24 10:41:24 -0700

    completed practice problem

---
+++ Senior Year/AP Computer Science/Unit Two/Practice/CanDrive.java
@@ -0,0 +1,39 @@
+/*
+ *
+ * Written by: Michael Brodskiy
+ * Class: AP Computer Science A
+ * Instructor: Mr. Davis
+ *
+ */
+
+/**
+ * This program takes in an age and states whether someone of that age can drive
+ **/
+
+import java.util.*;
+
+public class CanDrive
+
+{
+
+    public static void main(String[] args)
+    
+    {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Please enter your age: ");
+        int age = keyboard.nextInt();
+
+        if (age >= 16) {
+            
+            System.out.println("You can drive.");
+            System.exit(0);
+
+        }
+
+        System.out.println("You can not drive.");
+
+    }
+
+}
