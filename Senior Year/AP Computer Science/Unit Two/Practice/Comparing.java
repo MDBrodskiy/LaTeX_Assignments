aedd16e1b5d7a5d2b3e9f80b1aea8a8c181cfed9
Michael Brodskiy <Michael@Brodskiy.com>
2021-08-30 09:54:59 -0700

    completed practice

---
+++ Senior Year/AP Computer Science/Unit Two/Practice/Comparing.java
@@ -0,0 +1,37 @@
+/*
+ *
+ * Written by: Michael Brodskiy
+ * Class: AP Computer Science A
+ * Instructor: Mr. Davis
+ *
+ */
+
+/**
+ * This program is practice for the compareTo String method
+ **/
+
+import java.util.*;
+
+public class Comparing
+
+{
+
+    public static void main(String[] args)
+    
+    {
+
+        System.out.println("a".compareTo("f"));
+        System.out.println("a".compareTo("e"));
+        System.out.println("a".compareTo("d"));
+        System.out.println("a".compareTo("c"));
+        System.out.println("a".compareTo("b"));
+        System.out.println("a".compareTo("a"));
+        System.out.println("b".compareTo("a"));
+        System.out.println("c".compareTo("a"));
+        System.out.println("d".compareTo("a"));
+        System.out.println("e".compareTo("a"));
+        System.out.println("f".compareTo("a"));
+
+    }
+
+}
