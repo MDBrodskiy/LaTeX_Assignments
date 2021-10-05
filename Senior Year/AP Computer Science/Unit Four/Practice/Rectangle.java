935e204964439ae5383b109e9c5ca9381ee892eb
Michael Brodskiy <Michael@Brodskiy.com>
2021-10-05 10:56:05 -0700

    created rectangle clas

---
+++ Senior Year/AP Computer Science/Unit Four/Practice/Rectangle.java
@@ -0,0 +1,124 @@
+/*
+ *
+ * Written by: Michael Brodskiy
+ * Class: AP Computer Science A
+ * Instructor: Mr. Davis
+ *
+ */
+
+/**
+ * This program contains the rectangle class
+ **/
+
+import java.util.*;
+
+public class Rectangle
+
+{
+
+    private int width, height;
+
+    public Rectangle() {
+
+        this.width = 0;
+        this.height = 0;
+
+    }
+
+    public Rectangle(int w, int h) {
+
+        if (w < 0) {
+
+            this.width = 0;
+
+        }
+
+        else {
+
+            this.width = w;
+
+        }
+
+        if (h < 0) {
+
+            this.height = 0;
+
+        }
+        
+        else {
+
+            this.height = h;
+
+        }
+
+    }
+
+    public void setWidth(int w) {
+
+        if (w < 0) {
+
+            this.width = 0;
+
+        }
+
+        else {
+
+            this.width = w;
+
+        }
+
+    }
+
+    public void setHeight(int h) {
+
+        if (h < 0) {
+
+            this.height = 0;
+
+        }
+
+        else { 
+            
+            this.height = h;
+
+        }
+
+    }
+
+    public int getWidth() {
+
+        return this.width;
+
+    }
+
+    public int getHeight() {
+
+        return this.height;
+
+    }
+
+    public String toString() {
+
+        return "Width: " + this.width + ", Height: " + this.height;
+
+    }
+
+    public boolean equals(Rectangle other) {
+
+        if (this.width == other.getWidth() && this.height == other.getHeight()) {
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+    public boolean isSquare() {
+
+        return (this.height == this.width);
+
+    }
+
+}
